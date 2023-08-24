ce85ef715685f83eb8349657bcbf520a46159310
Devanshi Desai <contact.devanshi.desai@gmail.com>
2023-08-24 10:56:10 +0530

    Set up removed

--- src/app/courses/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./page.module.css";
-
-const Courses = () => {
-  return <div className={styles.container}>Courses</div>;
-};
-
-export default Courses;
